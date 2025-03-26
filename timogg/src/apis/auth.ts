c415af4346707b3d82e33fed9499a3642a43059e
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-03-26 10:28:11 +0900

    feat: 디스코드 로그인 구현

--- timogg/src/apis/auth.ts
+++ timogg/src/apis/auth.ts
@@ -7,7 +7,10 @@ export const socialLoginCallback = async (provider: string) => {
       window.location.href = `http://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_NAVER_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_NAVER_REDIRECT_URI}&state=${process.env.REACT_APP_NAVER_STATE}`;
       break;
     case 'kakao':
-      window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_RECIRECT_URI}&response_type=code`;
+      window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+      break;
+    case 'discord':
+      window.location.href = `https://discord.com/oauth2/authorize?client_id=${process.env.REACT_APP_DISCORD_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_DISCORD_REDIRECT_URI}&scope=email+identify`;
       break;
     default:
       break;
@@ -43,7 +46,17 @@ export const socialLogin = async (
         window.location.href = '/';
         throw error;
       }
-      break;
+    case 'discord':
+      try {
+        const response = await axiosInstance.post('/auth/discord', {
+          authorizationCode,
+        });
+        return response.data;
+      } catch (error) {
+        alert('로그인에 실패했습니다.');
+        window.location.href = '/';
+        throw error;
+      }
     default:
       return { accessToken: '', refreshToken: '' };
   }
